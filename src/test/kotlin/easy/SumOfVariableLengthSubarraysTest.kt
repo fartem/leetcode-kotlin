3589a321590e129ee63ecba94cac35f5ad35c68c
fartem <jaman.smlnsk@gmail.com>
2025-06-11 07:22:19 +0300

    2025-06-11 v. 1.0.5: added "3427. Sum of Variable Length Subarrays"

---
+++ src/test/kotlin/easy/SumOfVariableLengthSubarraysTest.kt
@@ -0,0 +1,28 @@
+package easy
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SumOfVariableLengthSubarraysTest {
+    private val solution = SumOfVariableLengthSubarrays()
+
+    @Test
+    fun testDefaultOne() {
+        assertEquals(
+            11,
+            solution.subarraySum(
+                intArrayOf(2, 3, 1)
+            )
+        )
+    }
+
+    @Test
+    fun testDefaultTwo() {
+        assertEquals(
+            13,
+            solution.subarraySum(
+                intArrayOf(3, 1, 1, 2)
+            )
+        )
+    }
+}
