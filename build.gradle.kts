620b18ab032a4e282a5916723498afb51df69065
fartem <jaman.smlnsk@gmail.com>
2025-06-12 11:42:04 +0300

    2025-06-12 v. 1.0.6: moved all files to new package

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "xyz.fartem"
-version = "1.0.5"
+version = "1.0.6"
 
 repositories {
     mavenCentral()
--- src/test/kotlin/xyz/fartem/leetcodekotlin/easy/FindCommonElementsBetweenTwoArraysTest.kt
+++ src/test/kotlin/xyz/fartem/leetcodekotlin/easy/FindCommonElementsBetweenTwoArraysTest.kt
@@ -1,4 +1,4 @@
-package easy
+package xyz.fartem.leetcodekotlin.easy
 
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
